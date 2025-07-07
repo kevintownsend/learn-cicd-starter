66d8b81f5905d3ec083a9e3ab8f33dca717db521
Kevin Townsend <kevintownsend4219@gmail.com>
2025-07-07 12:33:41 -0700

    Add style check.

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,20 +1,20 @@
 package auth
 
 import (
-  "reflect"
-  "testing"
+	"reflect"
+	"testing"
 )
 
 func TestAuth(t *testing.T) {
-  header := make(map[string][]string)
+	header := make(map[string][]string)
 
-  s, e := GetAPIKey(header)
-  if !reflect.DeepEqual(s, "") {
-    // TODO: Complete
-    t.Fatalf("expected: got:")
-  }
-  if e != ErrNoAuthHeaderIncluded {
-    // TODO: Complete
-    t.Fatalf("expected: got:")
-  }
+	s, e := GetAPIKey(header)
+	if !reflect.DeepEqual(s, "") {
+		// TODO: Complete
+		t.Fatalf("expected: got:")
+	}
+	if e != ErrNoAuthHeaderIncluded {
+		// TODO: Complete
+		t.Fatalf("expected: got:")
+	}
 }
