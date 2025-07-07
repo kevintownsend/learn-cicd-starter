110ed95e4c390b55e6a717fe79b3dc79c7aabdc5
Kevin Townsend <kevintownsend4219@gmail.com>
2025-07-07 10:27:10 -0700

    Fix broken test.

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+  "reflect"
+  "testing"
+)
+
+func TestAuth(t *testing.T) {
+  header := make(map[string][]string)
+
+  s, e := GetAPIKey(header)
+  if !reflect.DeepEqual(s, "") {
+    // TODO: Complete
+    t.Fatalf("expected: got:")
+  }
+  if e != ErrNoAuthHeaderIncluded {
+    // TODO: Complete
+    t.Fatalf("expected: got:")
+  }
+}
